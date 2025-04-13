6cd4226e6dc67e47f87a1b2419cc56a3c2f0dc08
J00NQ <kjk020913@gmail.com>
2025-04-13 20:27:31 +0900

    Reapply "0413_Test&README"
    This reverts commit 042b2e7028bf2a5df8516259faca62722aa81966.

--- data/quizTypes/baseQuiz.js
+++ data/quizTypes/baseQuiz.js
@@ -1,40 +1,63 @@
+const { createError, handleError, ErrorCodes } = require('../utils/errorHandler');
+const { QuestionValidator } = require('../validators/questionValidator');
+
 class BaseQuiz {
     constructor(quizData) {
+        if (!quizData) {
+            throw createError('퀴즈 데이터가 제공되지 않았습니다.', ErrorCodes.DATA);
+        }
+
+        if (!quizData.id || typeof quizData.id !== 'number') {
+            throw createError('유효하지 않은 퀴즈 ID입니다.', ErrorCodes.DATA);
+        }
+
         this.id = quizData.id;
-        this.title = quizData.title;
-        this.category = quizData.category;
-        this.description = quizData.description;
-        this.thumbnail = quizData.thumbnail;
+        this.title = quizData.title || '제목 없음';
+        this.description = quizData.description || '';
     }
 
     // 퀴즈 기본 정보 반환
     getQuizInfo() {
-        return {
-            id: this.id,
-            title: this.title,
-            category: this.category,
-            description: this.description,
-            thumbnail: this.thumbnail
-        };
+        try {
+            return {
+                id: this.id,
+                title: this.title,
+                description: this.description
+            };
+        } catch (error) {
+            return handleError(error, 'BaseQuiz.getQuizInfo');
+        }
     }
 
     // 현재 문제 정보 가져오기 (자식 클래스에서 구현)
     getQuestionData(question, options = null) {
-        throw new Error('getQuestionData must be implemented');
+        throw createError(
+            'getQuestionData 메서드가 구현되지 않았습니다.',
+            ErrorCodes.PROCESSING,
+            { className: this.constructor.name }
+        );
     }
 
     // 정답 검증 (자식 클래스에서 구현)
     validateAnswer(userAnswer, correctAnswer, options = null) {
-        throw new Error('validateAnswer must be implemented');
+        throw createError(
+            'validateAnswer 메서드가 구현되지 않았습니다.',
+            ErrorCodes.PROCESSING,
+            { className: this.constructor.name }
+        );
     }
 
-    // 결과 처리 (공통)
+    // 결과 처리 (자식 클래스에서 재정의 가능)
     processResult(isCorrect, correctAnswer) {
         return {
             isCorrect,
-            correctAnswer,
-            message: isCorrect ? '정답입니다!' : '틀렸습니다.'
+            correctAnswer
         };
+    }
+
+    // 유효성 검사 헬퍼 메서드
+    validateQuestionData(question) {
+        return QuestionValidator.validate(question, this.constructor.name.toLowerCase().replace('quiz', ''));
     }
 }
 
